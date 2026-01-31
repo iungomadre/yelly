2fb6039702007f1b89562a8f4dee7a5d98b397fb
Filip Wiśniewski <fi.wis@pm.me>
2026-01-31 19:08:56 +0100

    Preview webcam video

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,25 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
+import { useEffect, useRef } from 'preact/hooks'
 
 export function App() {
-  const [count, setCount] = useState(0)
+
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (videoRef.current) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(stream => {
+          videoRef.current!.srcObject = stream;
+          videoRef.current?.play();
+        })
+    }
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <h1>Test video streaming first</h1>
+      <video id='video' ref={videoRef}></video>
+      <canvas id='canvas'></canvas>
     </>
   )
 }
